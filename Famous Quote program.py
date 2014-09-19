eb168e6f03bfe65f1c70047adeb7dc75fe5ba416
JH101 <41425@longroad.ac.uk>
2014-09-19 13:38:00 +0100

    Work done week starting 15/09/14

---
+++ Famous Quote program.py
@@ -0,0 +1,30 @@
+# Jamie Hilton
+# 17/09/14
+# Famous Qoute Program v1.1
+
+person = input("Please enter the person who said the qoute: " )
+
+quote = input("Please enter the quote: ")
+
+quote_upper_case = quote.upper()
+
+print("{0} -{1}".format(quote_upper_case, person))
+
+quote_lower_case = quote.lower()
+
+print("{0} -{1}".format(quote_lower_case, person))
+
+quote_capitalize = quote.capitalize()
+
+print("{0} -{1}".format(quote_capitalize, person))
+
+quote_title = quote.title()
+
+print("{0} -{1}".format(quote_title, person))
+
+
+
+
+
+
+
